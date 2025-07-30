403ccbea41d7e698ec80c60aa8a1a777637ccd0a
ahmad <ahmad_almujawer@icloud.com>
2025-07-30 19:31:22 +0200

    Add state management and event handling for search input field

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import styles from "./SearchField.module.css";
 
 const SearchField = () => {
+  const [searchInput, setSearchInput] = useState("");
+  
+  const handleChange = (e) => {
+    const userInput = e.target.value;
+    setSearchInput(userInput);
+  }
   
   return (
     <div className={styles.wrapper}>
       <input
         placeholder="Filter dishes..."
         type="text"     
+        onChange={handleChange}
+        value={searchInput}
       />
     </div>
   );
