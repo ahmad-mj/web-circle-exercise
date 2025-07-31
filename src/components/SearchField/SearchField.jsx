3308144a445b800fd8a84fce848ebd5a9b3e0e01
ahmad <ahmad_almujawer@icloud.com>
2025-07-31 11:06:48 +0200

    Add filtering functionality to SearchField component using static array for testing purpose

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import styles from "./SearchField.module.css";
+const meals = [{id: "1", mealName:"flafel" },{ id: "2", mealName:"hummus" }];
 
 const SearchField = () => {
   const [searchInput, setSearchInput] = useState("");
-  
+  const [filterdMeals, setFilteredMeals] = useState(meals);
   const handleChange = (e) => {
     const userInput = e.target.value;
     setSearchInput(userInput);
-  }
   
+  console.log("searchInput: ",searchInput);
+
+  const filterMeals = meals.filter(meal => meal.mealName.includes(userInput))
+  setFilteredMeals(filterMeals)
+
+  }
   return (
     <div className={styles.wrapper}>
       <input
@@ -17,6 +23,7 @@ const SearchField = () => {
         onChange={handleChange}
         value={searchInput}
       />
+      <ul>{filterdMeals.map(meal => <li key={meal.id}>{meal.mealName}</li>)}</ul>
     </div>
   );
 };
